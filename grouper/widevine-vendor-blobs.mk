4b0d1b14b3a9f8796275062471103fdb488dde8a
Ziyan <jaraidaniel@gmail.com>
2016-02-18 20:44:58 +0100

    Update DRM blobs from sprout

--- grouper/widevine-vendor-blobs.mk
+++ grouper/widevine-vendor-blobs.mk
@@ -1,6 +1,9 @@
 VENDOR_FOLDER := vendor/widevine/grouper
 
 PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libdrmmtkutil.so:system/vendor/lib/libdrmmtkutil.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
