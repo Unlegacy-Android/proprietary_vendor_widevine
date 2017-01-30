d48e86503d13e3b6838b762329e1210a5e6bf120
Ziyan <jaraidaniel@gmail.com>
2017-01-30 21:36:08 +0100

    Add common Tegra 3 blobs
    Nexus 7 (Wi-Fi) binaries for Android 4.4.4 (KTU84P).
    
    Change-Id: Ieeb80adab7e6e25dd72698064648be0f4e07aa74

---
+++ tegra3/widevine-vendor-blobs.mk
@@ -0,0 +1,8 @@
+VENDOR_FOLDER := vendor/widevine/tegra3
+
+PRODUCT_COPY_FILES += \
+    $(VENDOR_FOLDER)/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
+    $(VENDOR_FOLDER)/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    $(VENDOR_FOLDER)/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    $(VENDOR_FOLDER)/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    $(VENDOR_FOLDER)/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
---
+++ tegra3/widevine-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/widevine/tegra3/widevine-vendor-blobs.mk)
