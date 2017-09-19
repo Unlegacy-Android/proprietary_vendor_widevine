f49128b0ed785b375bd242a987e9700c198b3da2
Ziyan <jaraidaniel@gmail.com>
2017-09-19 23:21:42 +0200

    Update DRM blobs from angler/OPR6.170623.013
    Change-Id: I69248c5df31d2ce2d3b5075454cb6b7d92435467

--- arm-generic/widevine-vendor.mk
+++ arm-generic/widevine-vendor.mk
@@ -3,8 +3,4 @@ VENDOR_WV_PATH := vendor/widevine/arm-generic
 $(call inherit-product-if-exists, $(VENDOR_WV_PATH)/widevine-vendor-blobs.mk)
 
 PRODUCT_PACKAGES += \
-	libdrmwvmplugin \
-	libwvdrmengine \
-	libwvdrm_L3 \
-	libwvm \
-	libWVStreamControlAPI_L3
+	libwvdrmengine
